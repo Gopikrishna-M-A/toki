9be6e0461d838f0a1e14c5168d44962d51ea14ae
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-07-14 17:22:56 +0530

    added cookie settings

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -20,4 +20,16 @@
         return session;
       },
     },
+    cookies: {
+      sessionToken: {
+        name: `next-auth.session-token`,
+        options: {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+          secure: true
+        }
+      },
+      // ... other cookie configurations
+    },
   }
