92ea855e8241ce8e8e18017bc26b32adc0d49d14
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-07-14 16:32:36 +0530

    added logs

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,9 @@ import { NextResponse } from 'next/server';
 
 export default async function middleware(req) {
   const path = req.nextUrl.pathname;
+  console.log("path",path);
   const session = !!req.cookies.get("next-auth.session-token")
-
+  console.log("session",session);
   if (!session) {
     return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
   }
