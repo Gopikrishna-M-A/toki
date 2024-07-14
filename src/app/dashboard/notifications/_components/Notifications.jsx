e79c68218da697df49c01c569830bede4af27125
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-07-14 16:04:13 +0530

    signin issue

--- src/app/dashboard/notifications/_components/Notifications.jsx
+++ src/app/dashboard/notifications/_components/Notifications.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import MessageCard from './MessageCard'
 
 const Notifications = ({notifications}) => {
-  console.log(notifications);
   return (
     <div className='grid grid-cols-2 gap-2'>
       {notifications.map((notification,index) => (
