685b9e0f22353448c44ca68987a4660814f081bb
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-07-16 15:46:59 +0530

    added modals for partner btn

--- src/app/dashboard/discover/_components/Partner.jsx
+++ src/app/dashboard/discover/_components/Partner.jsx
@@ -8,16 +8,26 @@ import Link from "next/link"
 import axios from "axios"
 import { useSession } from "next-auth/react"
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import Image from "next/image"
+
 const Partner = ({ business, perks, status }) => {
   const [currentPage, setCurrentPage] = useState(1)
-  const { data:session } = useSession()
+  const { data: session } = useSession()
   const [buttonState, setButtonState] = useState({
-    text: 'Send Partner Request',
-    disabled: false
-  });
-  const totalPages = 8 
+    text: "Send Partner Request",
+    disabled: false,
+  })
+  const totalPages = 8
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
-  const mapUri = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=place_id:${business.placeId}`;
+  const mapUri = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=place_id:${business.placeId}`
   const handlePrevPage = () => {
     setCurrentPage((prev) => Math.max(prev - 1, 1))
   }
@@ -25,48 +35,47 @@ const Partner = ({ business, perks, status }) => {
     setCurrentPage((prev) => Math.min(prev + 1, totalPages))
   }
 
-  useEffect(()=>{
-    updateButtonState();
-  },[])
+  useEffect(() => {
+    updateButtonState()
+  }, [])
 
   const updateButtonState = async () => {
     try {
-      
-      switch(status) {
-        case 'PENDING':
-          setButtonState({ text: 'Request Pending', disabled: true });
-          break;
-        case 'ACCEPTED':
-          setButtonState({ text: 'Already Partners', disabled: true });
-          break;
-        case 'REJECTED':
-          setButtonState({ text: 'Request Again', disabled: false });
-          break;
+      switch (status) {
+        case "PENDING":
+          setButtonState({ text: "Request Pending", disabled: true })
+          break
+        case "ACCEPTED":
+          setButtonState({ text: "Already Partners", disabled: true })
+          break
+        case "REJECTED":
+          setButtonState({ text: "Request Again", disabled: false })
+          break
         case null:
-          setButtonState({ text: 'Lets Partner', disabled: false });
-          break;
+          setButtonState({ text: "Lets Partner", disabled: false })
+          break
         default:
-          console.log('Unexpected status:', status);
-          setButtonState({ text: 'Lets Partner', disabled: false });
+          console.log("Unexpected status:", status)
+          setButtonState({ text: "Lets Partner", disabled: false })
       }
     } catch (error) {
-      console.error('Error updating button state:', error);
-      setButtonState({ text: 'Error', disabled: true });
+      console.error("Error updating button state:", error)
+      setButtonState({ text: "Error", disabled: true })
     }
-  };
+  }
 
   const handleNotification = async () => {
-    try {
-      const res = await axios.post('/api/notifications', {
-        senderBusinessId: session?.user?.businessId,
-        receiverBusinessId: business._id,
-        type: 'partner_request',
-        message: `I would like to partner with your business, ${business.name}`
-      })
-      setButtonState({ text: 'Request Pending', disabled: true });
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   const res = await axios.post('/api/notifications', {
+    //     senderBusinessId: session?.user?.businessId,
+    //     receiverBusinessId: business._id,
+    //     type: 'partner_request',
+    //     message: `I would like to partner with your business, ${business.name}`
+    //   })
+    //   setButtonState({ text: 'Request Pending', disabled: true });
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   return (
@@ -74,18 +83,49 @@ const Partner = ({ business, perks, status }) => {
       <div>
         <div className='flex justify-between items-center'>
           <Link href='/dashboard/discover'>
-          <Button variant='ghost'>
-            <ChevronLeft className='mr-2 h-4 w-4' /> {business.name}
-          </Button>
+            <Button variant='ghost'>
+              <ChevronLeft className='mr-2 h-4 w-4' /> {business.name}
+            </Button>
           </Link>
-          <Button  disabled={buttonState.disabled} onClick={handleNotification}>{buttonState.text}</Button>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                disabled={buttonState.disabled}
+                onClick={handleNotification}>
+                {buttonState.text}
+              </Button>
+            </DialogTrigger>
+            <DialogContent className='sm:max-w-md p-0 py-5 bg-[#F7EBEB]'>
+              <DialogHeader>
+                <DialogTitle className='text-center text-xl font-medium text-indigo-900'>
+                  Hooray!
+                </DialogTitle>
+              </DialogHeader>
+              <div className="flex justify-center relative h-60 -mt-10 -z-10">
+                <Image
+                  src='/images/partnership.png'
+                  alt='Partnership Hands'
+                  layout="fill"
+                  className='w-auto h-auto left-0 right-0'
+                />
+              </div>
+              <div className='text-center text-xl -mt-14 text-indigo-900 mb-4'>
+                You have made a new partner
+              </div>
+              <div className='text-center text-sm -mt-6 font-light text-gray-600 px-5'>
+                Your coupons have been shared and linked to your member portal
+                for your customers to redeem.
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div className='grid grid-cols-5 w-full h-full gap-4'>
         <div className='bg-white col-span-2 rounded-lg py-10 px-5 flex flex-col gap-5'>
           <div className='text-lg font-light text-gray-800'>About</div>
           <div className='flex gap-2 items-center'>
-            <StarRating rating={business.rating}/>
+            <StarRating rating={business.rating} />
           </div>
           <div className='flex gap-10'>
             <div className='flex flex-col items-center'>
@@ -99,23 +139,20 @@ const Partner = ({ business, perks, status }) => {
           </div>
           <div className='flex flex-col gap-2'>
             <div className='text-sm text-gray-400'>company description</div>
-            <div className='text-sm text-gray-600'>
-             {business.description}
-            </div>
+            <div className='text-sm text-gray-600'>{business.description}</div>
           </div>
 
           <div className='flex flex-col gap-2'>
             <div className='text-sm text-gray-400'>Location</div>
             <div className='w-80 h-32 bg-gray-200 rounded-lg'>
-            <div className='w-80 h-32  rounded-lg overflow-hidden'>
-        <iframe
-          src={mapUri}
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen
-        ></iframe>
-      </div>
+              <div className='w-80 h-32  rounded-lg overflow-hidden'>
+                <iframe
+                  src={mapUri}
+                  width='100%'
+                  height='100%'
+                  style={{ border: 0 }}
+                  allowFullScreen></iframe>
+              </div>
             </div>
           </div>
         </div>
@@ -141,12 +178,10 @@ const Partner = ({ business, perks, status }) => {
               </button>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-2 mt-5">
-            {
-                perks.map((perk,index) => (
-                    <PerkCard key={index} perk={perk}/>
-                ))
-            }
+          <div className='grid grid-cols-2 gap-2 mt-5'>
+            {perks.map((perk, index) => (
+              <PerkCard key={index} perk={perk} />
+            ))}
           </div>
         </div>
       </div>
--- src/app/dashboard/discover/_components/PartnerMobile.jsx
+++ src/app/dashboard/discover/_components/PartnerMobile.jsx
@@ -4,90 +4,97 @@ import PerkCard from "./PerkCard"
 import { ChevronLeft, ChevronRight, StarIcon } from "lucide-react"
 import React, { useEffect, useState } from "react"
 import StarRating from "./StarRating"
-import {
-  Menubar,
-  MenubarMenu,
-  MenubarTrigger,
-} from "@/components/ui/menubar"
+import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar"
 import Image from "next/image"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Link from "next/link"
 import { useSession } from "next-auth/react"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 const PartnerMobile = ({ business, perks, status }) => {
   const [currentPage, setCurrentPage] = useState(1)
   const [menu, setMenu] = useState("About")
-  const { data:session } = useSession()
+  const { data: session } = useSession()
   const [buttonState, setButtonState] = useState({
-    text: 'Send Partner Request',
-    disabled: false
-  });
+    text: "Send Partner Request",
+    disabled: false,
+  })
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
   const mapUri = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=place_id:${business.placeId}`
 
-  useEffect(()=>{
-    updateButtonState();
-  },[])
+  useEffect(() => {
+    updateButtonState()
+  }, [])
 
   const updateButtonState = async () => {
     try {
-      
-      switch(status) {
-        case 'PENDING':
-          setButtonState({ text: 'Request Pending', disabled: true });
-          break;
-        case 'ACCEPTED':
-          setButtonState({ text: 'Already Partners', disabled: true });
-          break;
-        case 'REJECTED':
-          setButtonState({ text: 'Request Again', disabled: false });
-          break;
+      switch (status) {
+        case "PENDING":
+          setButtonState({ text: "Request Pending", disabled: true })
+          break
+        case "ACCEPTED":
+          setButtonState({ text: "Already Partners", disabled: true })
+          break
+        case "REJECTED":
+          setButtonState({ text: "Request Again", disabled: false })
+          break
         case null:
-          setButtonState({ text: 'Lets Partner', disabled: false });
-          break;
+          setButtonState({ text: "Lets Partner", disabled: false })
+          break
         default:
-          console.log('Unexpected status:', status);
-          setButtonState({ text: 'Lets Partner', disabled: false });
+          console.log("Unexpected status:", status)
+          setButtonState({ text: "Lets Partner", disabled: false })
       }
     } catch (error) {
-      console.error('Error updating button state:', error);
-      setButtonState({ text: 'Error', disabled: true });
+      console.error("Error updating button state:", error)
+      setButtonState({ text: "Error", disabled: true })
     }
-  };
+  }
 
   const handleNotification = async () => {
-    try {
-      const res = await axios.post('/api/notifications', {
-        senderBusinessId: session?.user?.businessId,
-        receiverBusinessId: business._id,
-        type: 'partner_request',
-        message: `I would like to partner with your business, ${business.name}`
-      })
-      setButtonState({ text: 'Request Pending', disabled: true });
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   const res = await axios.post('/api/notifications', {
+    //     senderBusinessId: session?.user?.businessId,
+    //     receiverBusinessId: business._id,
+    //     type: 'partner_request',
+    //     message: `I would like to partner with your business, ${business.name}`
+    //   })
+    //   setButtonState({ text: 'Request Pending', disabled: true });
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   return (
     <div className='w-full h-full flex md:hidden gap-2 flex-col relative bg-white'>
-        <div className='absolute top-6 left-4'>
-          <Link href='/dashboard/discover'>
+      <div className='absolute top-6 left-4'>
+        <Link href='/dashboard/discover'>
           <Button variant='ghost' size='icon'>
             <ChevronLeft className='mr-2 h-4 w-4' />
           </Button>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2 justify-center items-center mt-4">
-            <div >
-            <Avatar className="w-20 h-20">
+        </Link>
+      </div>
+      <div className='flex flex-col gap-2 justify-center items-center mt-4'>
+        <div>
+          <Avatar className='w-20 h-20'>
             <AvatarImage src={business.logo} alt={business.name} />
-            <AvatarFallback className='font-bold text-3xl text-gray-400'>{business.name.charAt(0)}</AvatarFallback>
+            <AvatarFallback className='font-bold text-3xl text-gray-400'>
+              {business.name.charAt(0)}
+            </AvatarFallback>
           </Avatar>
-            </div>
-            <div className="font-semibold">{business.name}</div>
-            <StarRating rating={business.rating} />
         </div>
+        <div className='font-semibold'>{business.name}</div>
+        <StarRating rating={business.rating} />
+      </div>
       <Menubar defaultValue='About' className='w-fit mx-auto mt-4'>
         <MenubarMenu>
           <MenubarTrigger onClick={() => setMenu("About")}>
@@ -104,7 +111,7 @@ const PartnerMobile = ({ business, perks, status }) => {
       {menu == "About" && (
         <div className='flex flex-col w-full  gap-4'>
           <div className='bg-white rounded-lg py-5 px-5 flex flex-col gap-5'>
-          <div className='flex flex-col gap-2'>
+            <div className='flex flex-col gap-2'>
               <div className='text-sm text-gray-400'>company description</div>
               <div className='text-sm text-gray-600'>
                 {business.description}
@@ -120,18 +127,17 @@ const PartnerMobile = ({ business, perks, status }) => {
                 <div className='text-4xl font-bold'>239</div>
               </div>
             </div>
-           
 
             <div className='flex flex-col gap-2 mt-4'>
               <div className='text-sm text-gray-400'>Location</div>
-                <div className='w-full h-36 bg-gray-200 rounded-lg overflow-hidden'>
-                  <iframe
-                    src={mapUri}
-                    width='100%'
-                    height='100%'
-                    style={{ border: 0 }}
-                    allowFullScreen></iframe>
-                </div>
+              <div className='w-full h-36 bg-gray-200 rounded-lg overflow-hidden'>
+                <iframe
+                  src={mapUri}
+                  width='100%'
+                  height='100%'
+                  style={{ border: 0 }}
+                  allowFullScreen></iframe>
+              </div>
             </div>
           </div>
         </div>
@@ -147,7 +153,36 @@ const PartnerMobile = ({ business, perks, status }) => {
           </div>
         </div>
       )}
-      <Button className='max-w-sm w-full mx-auto'  disabled={buttonState.disabled} onClick={handleNotification}>{buttonState.text}</Button>
+
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button
+            className='max-w-sm w-full mx-auto'
+            disabled={buttonState.disabled}
+            onClick={handleNotification}>
+            {buttonState.text}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className='p-7'>
+          <DrawerHeader></DrawerHeader>
+
+          <div className='flex justify-center h-32 mb-10'>
+            <Image
+              src='/images/partnership2.png'
+              alt='Partnership Hands'
+              width={200}
+              height={200}
+              className='object-contain'
+            />
+          </div>
+          <div className='text-center text-xl font-medium text-gray-600 px-10 pb-5'>
+             Hooray!
+          </div>
+          <div className='text-center font-light text-gray-600 px-10 pb-5'>
+            Your coupons have been shared and linked to your members portal.
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   )
 }
